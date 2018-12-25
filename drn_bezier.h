e07ce3f6e2f1c29f7c2c9d3f656202e19e20c138
Dan Nielsen <none@example.com>
2018-12-24 19:06:40 -0800

    added a stub file for bezier lib

---
+++ drn_bezier.h
@@ -0,0 +1,36 @@
+#ifndef __INCLUDED__DRN_BEZIER_H__
+#define __INCLUDED__DRN_BEZIER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __INCLUDED__DRN_BEZIER_H__
+
+
+
+// ***************************** IMPLEMENTATION *******************************
+#ifdef DRN_BEZIER_IMPLEMENTATION
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-compare"
+
+#include <math.h>
+
+
+
+
+
+
+
+#pragma GCC diagnostic pop
+
+#endif /* DRN_BEZIER_IMPLEMENTATION */
