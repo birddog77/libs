8b0d70f8106dabd59ad89ed2f8bf4879602d5acd
Dan Nielsen <none@example.com>
2018-12-24 20:10:06 -0800

    addded a bezier2d func

--- drn_bezier.h
+++ drn_bezier.h
@@ -5,7 +5,12 @@
 extern "C" {
 #endif
 
+struct BEZ_POINT2D {
+   float x;
+   float y;
+};
 
+float drn_bezier_cubic2D(float t, BEZ_POINT2D control[4]);
 
 
 
@@ -25,7 +30,18 @@ extern "C" {
 
 #include <math.h>
 
-
+BEZ_POINT2D drn_bezier_cubic2D(float t, BEZ_POINT2D w[4])
+{
+   BEZ_POINT2D res;
+   float t2 = t*t;
+   float t3 = t2*t;
+   float mt = 1.f - t;
+   float mt2 = mt*mt;
+   float mt3 = mt2*mt;
+   res.x = w[0].x*mt3 + 3*w[1].x*mt2*t + 3*w[2].x*mt*t2 + w[3].x*t3;
+   res.y = w[0].y*mt3 + 3*w[1].y*mt2*t + 3*w[2].y*mt*t2 + w[3].y*t3;
+   return res;
+}
 
 
 
